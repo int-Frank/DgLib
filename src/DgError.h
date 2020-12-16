25c09a936d38a49a6d2e27ce2bd55eed13607461
Frank Hart <frankhart010@gmail.com>
2020-12-17 08:11:54 +1000

    Added some more error values

--- src/DgError.h
+++ src/DgError.h
@@ -10,8 +10,8 @@
 
 #include <stdint.h>
 
-#define DG_ERROR_IF(cond, code)     do { if (cond)           { result = code; if (result != ::Dg::ErrorCode::None) goto epilogue; } while(0)
-#define DG_ERROR_NULL(ptr, code)    do { if (ptr == nullptr) { result = code; if (result != ::Dg::ErrorCode::None) goto epilogue; } while(0)
+#define DG_ERROR_IF(cond, code)     do { if (cond)           { result = code; if (result != ::Dg::ErrorCode::None) goto epilogue; }} while(0)
+#define DG_ERROR_NULL(ptr, code)    do { if (ptr == nullptr) { result = code; if (result != ::Dg::ErrorCode::None) goto epilogue; }} while(0)
 #define DG_ERROR_SET(code)          do { result = code;                       if (result != ::Dg::ErrorCode::None) goto epilogue; } while(0)
 #define DG_ERROR_CHECK(funcCall)    do { result = funcCall; if (result != ::Dg::ErrorCode::None) goto epilogue; } while(0)
 
@@ -24,13 +24,15 @@
   ITEM(OutOfBounds) \
   ITEM(Duplicate) \
   ITEM(FailedToOpenFile) \
+  ITEM(IncorrectFileType) \
   ITEM(BadInput) \
   ITEM(StreamNotOpen) \
   ITEM(FailedToAllocMem) \
   ITEM(Disallowed) \
   ITEM(InsufficientSpace) \
   ITEM(NullObject) \
-  ITEM(WriteError)
+  ITEM(WriteError) \
+  ITEM(FailedToInitialise)
 
 namespace Dg
 {
