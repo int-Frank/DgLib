0ff54fc1a7a62a9f7204b51e737d43a0ad0eb593
Frank Hart <frankhart010@gmail.com>
2023-04-26 03:55:50 +1000

    FI Polygon vs polygon, not tested

---
+++ src/impl/DgQueryPolygonPolygon.cpp
@@ -0,0 +1,32 @@
+
+#include "../DgQueryPolygonPolygon.h"
+
+namespace Dg
+{
+  namespace impl_FI2PolygonPolygon
+  {
+    EdgePair::EdgePair(uint64_t e0, uint64_t e1)
+      : edge0(e0)
+      , edge1(e1)
+    {
+      if (e0 > e1)
+      {
+        edge0 = e1;
+        edge1 = e0;
+      }
+    }
+
+    bool EdgePairLess(EdgePair const &p0, EdgePair const &p1)
+    {
+      if (p0.edge0 < p1.edge0)
+        return true;
+      if (p0.edge0 > p1.edge0)
+        return false;
+      if (p0.edge1 < p1.edge1)
+        return true;
+      if (p0.edge1 > p1.edge1)
+        return false;
+      return false; // equal
+    }
+  }
+}
